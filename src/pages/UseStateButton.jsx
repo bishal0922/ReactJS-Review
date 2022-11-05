732ee40759616dedf2b3eabf834cc5ec1a9b7ae2
Bishal Giri <bishal0922@gmail.com>
2022-11-05 01:38:33 -0500

    added the jsx for the card

--- src/pages/UseStateButton.jsx
+++ src/pages/UseStateButton.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useState } from 'react'
+import './UseStateButton.css'
 
 const UseStateButton = () => {
   const [count, setCount] = React.useState(0)
   const [show, setShow] = React.useState(true)
 
   return (
-    <div>
-      <h1>UseStatebutton {show &&<p>clicked {count} times</p>}</h1> 
+    <div className='usb'>
+      {/* <h1>UseStatebutton {show &&<p>clicked {count} times</p>}</h1> 
       
       <p>Some informatin about use state button</p>
       <hr>
@@ -16,7 +17,25 @@ const UseStateButton = () => {
       <button onClick={() => setCount(count + 1)}>Click Me to Add</button>
       <button onClick={() => setCount(count - 1)}>Click Me to Sub</button>
       <button onClick={() => setCount(0)}>Click Me to Reset</button>
-      <button onClick={() => setShow(!show)}>Click Me to Hide</button>
+      <button onClick={() => setShow(!show)}>Click Me to Hide</button> */}
+
+      <div className="usb-container">
+        <div className="usb-container-top" >
+          <div className='usb-container-top-text'>
+            <p>Count</p>
+          </div>       
+          <div className='usb-container-top-num'>
+            <p>{count}</p>
+          </div>
+        </div>
+
+
+        {/* <div className="usb-container-middle">
+          <p>Some information about use state button</p>
+        </div> */}
+
+        {/* something for the middle and bottom */}
+      </div>
     </div>
   )
 }
