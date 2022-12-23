897da90dd65d023308b9a74a62fbc9a7980f3922
Bishal Giri <bishal0922@gmail.com>
2022-12-23 12:51:01 -0700

    add the jsx for usestate button

--- src/pages/UseStateButton.jsx
+++ src/pages/UseStateButton.jsx
@@ -19,23 +19,49 @@ const UseStateButton = () => {
       <button onClick={() => setCount(0)}>Click Me to Reset</button>
       <button onClick={() => setShow(!show)}>Click Me to Hide</button> */}
 
-      <div className="usb-container">
+      {/* Main card to display the Count and buttons*/}
+      <div className="usb-container"> 
+
+        {/* Top card to show the Count - want to refactor this code to not have other divs*/}
         <div className="usb-container-top" >
+
+          {/* Text "Counnt" */}
           <div className='usb-container-top-text'>
             <p>Count</p>
           </div>       
-          <div className='usb-container-top-num'>
+
+          {/* Number portion of the top card */}
+          {show && <div className='usb-container-top-num'>
             <p>{count}</p>
-          </div>
+          </div>}
+
+        </div> 
+        {/* Top card end */}
+
+        {/* Middle card to shwo the controls */}
+        <div className='usb-container-middle'>
+        <p>The useState hook takes an initial state value as an argument and returns an array with two elements: the current state value, and a upate function.
+        You can use the state variable + function in your component's JSX to render the component based on the state. Here the update function is called from any of the four buttons below.
+          
+          </p>
         </div>
 
-        {/* Need to add a grid layout inside of usb-> usb-container-top /middle/bottom */}
+        <div className='usb-container-end'>
+          {/* need a +1 -1 =0  */}
+          {/* Hide the count */}
+          {/* two divs one for upper section and one for lower section
+              upper section will be a div with 3 paragraphs 
+          */}
+          <div className='usb-container-end-top'>
+            <button className='usb-container-end-top-p'onClick={() => setCount(count + 1)}>+1</button>
+            <button className='usb-container-end-top-p'onClick={() => setCount(count - 1)}>-1</button>
+            <button className='usb-container-end-top-p'onClick={() => setCount(0)}>=0</button>
+          </div>
 
-        {/* <div className="usb-container-middle">
-          <p>Some information about use state button</p>
-        </div> */}
-
-        {/* something for the middle and bottom */}
+          <div className='usb-container-end-bottom'>
+            <button onClick={() => setShow(!show)}>Toggle the Count Display</button>
+          </div>
+        </div>
 
       </div>
     </div>
